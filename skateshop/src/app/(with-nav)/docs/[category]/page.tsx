b8a4c94c33fdfd3243d69a9c5efc812534c20d87
makedoniaz <jafar.sadig04@gmail.com>
2025-01-18 18:25:24 +0400

    Add dynamic routing for pages in main page

---
+++ skateshop/src/app/(with-nav)/docs/[category]/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { RxRocket } from "react-icons/rx";
+
+interface Props {
+  params: {
+    category: string;
+  };
+}
+
+export default async function CategoryPage({ params }: Props) {
+  const { category } = await params;
+  return (
+    <div className="container mt-10 flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-3xl font-bold">
+          {category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          Buy {category} from the best stores
+        </p>
+      </div>
+    </div>
+  );
+}
