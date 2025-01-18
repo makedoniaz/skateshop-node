4375461521b26499c46ac4e3c1f851848bf064e8
makedoniaz <jafar.sadig04@gmail.com>
2025-01-18 19:13:20 +0400

    Implement category subsections navigation

---
+++ skateshop/src/app/(with-nav)/docs/[category]/[href]/page.tsx
@@ -0,0 +1,42 @@
+// import { navbar } from "@/data/navbar";
+
+import { NavBarProps } from "@/components/helpers/interfaces/navbar";
+
+interface PageProps {
+  params: Promise<{
+    href: string;
+    category: string;
+  }>;
+}
+
+export default async function ProductPage({ params }: PageProps) {
+  const { category, href } = await params;
+
+  const response = await fetch(`${process.env.API_HOST}/navbar`);
+
+  if (!response.ok) {
+    throw new Error("Failed to load navbar data");
+  }
+
+  const navbar: NavBarProps[] = await response.json();
+
+  const product = navbar
+    .flatMap((product) => product.items)
+    .find((item) => item.href === `/docs/${category}/${href}`);
+
+  if (!product)
+    return (
+      <div className="container">
+        <h1>Category not found</h1>
+      </div>
+    );
+
+  return (
+    <div className="container">
+      <h1>This is page from Category: {category}</h1>
+      <p>page url: {href}</p>
+      you are view product: Product title &quot;{product.title || "..."}&quot;
+      on url: &quot;{product.href}&quot;
+    </div>
+  );
+}
